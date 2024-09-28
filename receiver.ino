dc48dc54fc61b05a863f15cdd172df7aa9a7b6ed
linux-gamer <115344588+linux-gamer@users.noreply.github.com>
2024-09-28 10:34:56 +0200

    Update receiver.ino

--- receiver.ino
+++ receiver.ino
@@ -1,39 +1,65 @@
 #include <Arduino.h>
+
 #include <WiFi.h>
 #include <esp_now.h>
-#include <NTP.h>
-#include <WiFiUdp.h>
-#include <HTTPClient.h>
+#include <esp_wifi.h>
+#include "time.h"
+#include "RTClib.h"
 
-#include <Adafruit_GFX.h>  // Include core graphics library
-#include <Adafruit_ST7735.h>  // Include Adafruit_ST7735 library to drive the display
-#include <SPI.h>
+#include <TFT_eSPI.h>
 
-#include "DHT.h"
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+#include <Adafruit_AHTX0.h>
 
-#define DHTPIN 11     // Digital pin connected to the DHT sensor
-#define DHTTYPE DHT22 // DHT 22  (AM2302), AM2321
+#include "temperature_img.h"
+#include "water_drop.h"
 
-// Declare pins for the display:
-#define TFT_MOSI  3
-#define TFT_SCLK  2
-#define TFT_CS    7   // Chip select control pin
-#define TFT_DC    6   // Data Command control pin
-#define TFT_RST  10   // Reset pin (could connect to RST pin)
+#define SDA 27
+#define SCL 22
 
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+#define DCF77_PIN 35
 
-unsigned long last_milli;
-unsigned long last_sec;
+#define CYD_LED_BLUE 17
+#define CYD_LED_RED 4
+#define CYD_LED_GREEN 16
 
-const char* ssid = "wlan_ssid";
-const char* password = "wlan_password";
+const char* daysOfTheWeek[] = {"So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"};
+const char* months[] = {"Dez", "Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"};
 
-WiFiUDP wifiUdp;
-NTP ntp(wifiUdp);
- 
+unsigned long gotTimeMillis;
+bool print_time = false;
+volatile unsigned long lastInt = 0;
+volatile unsigned long long currentBuf = 0;
+volatile byte bufCounter;
+
+bool MEZ;
+byte dcf77Year;
+byte dcf77Month;
+byte dcf77DayOfWeek;
+byte dcf77DayOfMonth;
+byte dcf77Hour;
+byte dcf77Minute;
+bool parityBitMinute;
+bool parityBitHour;
+bool parityBitDate;
+
+bool print_temp;
+int esp_now_id;
+float esp_now_temp;
+float esp_now_hum;
+float esp_now_pres;
+
+RTC_DS3231 rtc;
+
+Adafruit_BMP280 bmp; // I2C
+Adafruit_AHTX0 aht;
+
+sensors_event_t humidity, temp;
+
 // Define data structure
-typedef struct temp_struct {
+struct temp_struct {
+  int id;
   float temp;
   float hum;
   float pres;
@@ -42,137 +68,262 @@ typedef struct temp_struct {
 // Create structured data object
 temp_struct tempData;
 
-DHT dht(DHTPIN, DHTTYPE);
+TFT_eSPI tft = TFT_eSPI();
 
-// Callback function
+bool IRAM_ATTR parity_even_bit(byte val){
+  val ^= val >> 4;
+  val ^= val >> 2;
+  val ^= val >> 1;
+  val &= 0x01;
+  return val;
+}
+
+unsigned int rawByteToInt(byte raw){
+  return ((raw>>4)*10 + (raw &0x0F));
+}
+
+void IRAM_ATTR evaluateSequence(){
+  print_time = true;
+
+  dcf77Year = (currentBuf>>50) & 0xFF;    // year = bit 50-57
+  dcf77Month = (currentBuf>>45) & 0x1F;       // month = bit 45-49
+  dcf77DayOfWeek = (currentBuf>>42) & 0x07;   // day of the week = bit 42-44
+  dcf77DayOfMonth = (currentBuf>>36) & 0x3F;  // day of the month = bit 36-41
+  dcf77Hour = (currentBuf>>29) & 0x3F;       // hour = bit 29-34
+  dcf77Minute = (currentBuf>>21) & 0x7F;     // minute = 21-27
+  MEZ = (currentBuf>>18) & 1;                      // MEZ = 1 MESZ = 0
+  parityBitMinute = (currentBuf>>28) & 1;
+  parityBitHour = (currentBuf>>35) & 1;
+  parityBitDate = (currentBuf>>58) & 1;
+
+  Serial.println(currentBuf);
+  
+  if (rawByteToInt(dcf77Month) > 12  || rawByteToInt(dcf77DayOfWeek) > 7 || rawByteToInt(dcf77DayOfMonth) > 31 || rawByteToInt(dcf77Hour) > 23 || rawByteToInt(dcf77Minute) > 59) {
+    print_time = false;
+  }
+
+  if(bufCounter != 59){
+    print_time = false;
+  }
+  if((parity_even_bit(dcf77Minute)) != parityBitMinute){
+    //Serial.println("Minute parity not OK"); 
+    print_time = false;
+  }
+  if((parity_even_bit(dcf77Hour)) != parityBitHour){
+    //Serial.println("Hour parity not OK");
+    print_time = false;
+  }
+  if(((parity_even_bit(dcf77DayOfMonth) + parity_even_bit(dcf77DayOfWeek) 
+  + parity_even_bit(dcf77Month) + parity_even_bit(dcf77Year))%2) != parityBitDate)
+  {
+    //Serial.println("Date parity not OK");
+    print_time = false;
+  }
+  gotTimeMillis = millis();
+}
+
+void IRAM_ATTR DCF77_ISR(){
+  unsigned long dur = 0;
+  dur = millis() - lastInt;
+  
+  if(digitalRead(DCF77_PIN)){
+    //Serial.println(dur);
+    if(dur>1500){
+      unsigned long highBuf = (currentBuf>>32) & 0x7FFFFFF;
+      unsigned long lowBuf = (currentBuf & 0xFFFFFFFF);
+      /*Serial.print("Signal, upper 4 bytes: "); 
+      Serial.println(highBuf, BIN);
+      Serial.print("Signal, lower 4 bytes: "); 
+      Serial.println(lowBuf, BIN); */
+      evaluateSequence();
+      bufCounter = 0;
+      currentBuf = 0;
+    }
+  }
+  else{
+    Serial.println(bufCounter);
+    /*Serial.print(". ");
+    Serial.print(dur);
+    Serial.print("  /  ");*/
+    if(dur>150){
+      currentBuf |= ((unsigned long long)1<<bufCounter);
+    }
+    bufCounter++;
+  }
+  lastInt = millis();
+}
+
+void printLocalTime() {
+  DateTime now = rtc.now();
+  if (now.hour() == 0 && now.minute() == 0 && now.second() == 0) {
+    tft.fillRect(0, 50, 320, 30, TFT_BLACK);
+  }
+
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+
+  char buf1[] = "hh:mm:ss";
+  String timeStr = now.toString(buf1);
+  String dateStr = String(daysOfTheWeek[now.dayOfTheWeek()]) + ". " + String(now.day(), DEC) + ". " + months[now.month()];
+
+  tft.drawCentreString(timeStr,  320/2, 0, 6);
+
+  tft.drawCentreString(dateStr,  320/2, 50, 4);
+}
+
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) 
 {
+  esp_now_peer_info_t peer_info;
   // Get incoming data
   memcpy(&tempData, incomingData, sizeof(tempData));
 
-  float temp = tempData.temp - 0.15;
-  float hum = tempData.hum;
-  float pres = tempData.pres + 11;
+  print_temp = true;
+  esp_now_id = tempData.id;
+  esp_now_temp = tempData.temp;
+  esp_now_hum = tempData.hum;
+  esp_now_pres = tempData.pres;
   
   // Print to Serial Monitor
+/*  Serial.println();
+
   Serial.print("Temperature: ");
-  Serial.println(temp);
+  Serial.println(esp_now_temp);
   Serial.print("Humidity: ");
-  Serial.println(hum);
+  Serial.println(esp_now_hum);
   Serial.print("Pressure: ");
-  Serial.println(pres);
-  String tempStr = String(temp, 2);
-  String humStr = String(hum, 1);
-  String presStr = String(pres, 2);
+  Serial.println(esp_now_pres);
 
-  tft.setTextColor(ST7735_ORANGE, ST7735_BLACK);  // Set color of text. First is the color of text and after is color of background
-  tft.setTextSize(1);  // Set text size. Goes from 0 (the smallest) to 20 (very big)
-
-  tft.setCursor(115, 40);  // Set position (x,y)
-  tft.println(tempStr + " C");
-  tft.setCursor(120, 50);  // Set position (x,y)
-  tft.println(humStr + " %");
-  tft.setCursor(90, 60);  // Set position (x,y)
-  tft.println(presStr + " hPa");
+  Serial.print("ESP NOW ID: ");
+  Serial.println(esp_now_id);*/
 }
 
+
+
 void setup() {
-  // Set up Serial Monitor
   Serial.begin(115200);
 
-  tft.initR(INITR_BLACKTAB);
-  tft.fillScreen(ST7735_BLACK);
-  tft.setRotation(1);
-  tft.setTextWrap(false);
-  
-  tft.setTextColor(ST7735_ORANGE);  // Set color of text. First is the color of text and after is color of background
-  tft.setTextSize(1);  // Set text size. Goes from 0 (the smallest) to 20 (very big)
+  pinMode(DCF77_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(DCF77_PIN), DCF77_ISR, CHANGE);
 
-  tft.setCursor(0, 30);  // Set position (x,y)
-  tft.println("Aussen: ");
-  tft.setCursor(8, 40);
-  tft.println("Temperatur:");
-  tft.setCursor(8, 50);
-  tft.println("Luftfeuchtigkeit:");
-  tft.setCursor(8, 60);
-  tft.println("Luftdruck:");
-  tft.setCursor(0, 70);
-  tft.println("Innen: ");
-  tft.setCursor(8, 80);
-  tft.println("Temperatur:");
-  tft.setCursor(8, 90);
-  tft.println("Luftfeuchtigkeit:");
- 
-  // Start ESP32 in Station mode
-  WiFi.mode(WIFI_AP_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("\nConnecting");
+  pinMode(CYD_LED_RED, OUTPUT);
+  pinMode(CYD_LED_GREEN, OUTPUT);
+  pinMode(CYD_LED_BLUE, OUTPUT);
+  digitalWrite(CYD_LED_RED, HIGH);
+  digitalWrite(CYD_LED_GREEN, HIGH);
+  digitalWrite(CYD_LED_BLUE, HIGH);
 
-  int count = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    ++count;
-    if (count > 50) {
-      ESP.restart();
-    }
-    Serial.print(".");
-    delay(100);
+  Wire.begin(SDA, SCL);
+
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+      Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+      Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+      Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+      Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    abort();
   }
 
-  Serial.println("\nConnected to the WiFi network");
-  Serial.print("IP: ");
-  Serial.println(WiFi.localIP());
+  if (!bmp.begin()) {
+    Serial.println("Could not find BMP? Check wiring");
+  }
 
-  /*
-    Zeitzone
-    CEST: Central European Summertime
-    Beginn europäische Sommerzeit letzter Sonntag im März 2 Uhr GMT + 2 Stunden
-  */
-  ntp.ruleDST("CEST", Last, Sun, Mar, 2, 120);
-  // CET: Central European Time
-  // Beginn Normalzeit letzter Sonntag im Oktober 3 Uhr GMT + 1 Stunde
-  ntp.ruleSTD("CET", Last, Sun, Oct, 3, 60);
-  // ntp starten
-  ntp.begin();
- 
-  dht.begin();
+  if (!aht.begin()) {
+    Serial.println("Could not find AHT? Check wiring");
+  }
 
-  // Initalize ESP-NOW
+  // Start the tft display and set it to black
+  tft.init();
+  //tft.invertDisplay(1); //If you have a CYD2USB - https://github.com/witnessmenow/ESP32-Cheap-Yellow-Display/blob/main/cyd.md#my-cyd-has-two-usb-ports
+  tft.setRotation(1); //This is the display in landscape
+  
+  // Clear the screen before writing to it
+  tft.fillScreen(TFT_BLACK);
+
+  tft.pushImage(0, 160, TEMPERATURE_IMG_WIDTH, TEMPERATURE_IMG_HEIGHT, temperature_img);
+
+  tft.setSwapBytes(true);
+  tft.pushImage(160, 160, TEMPERATURE_IMG_WIDTH, TEMPERATURE_IMG_HEIGHT, temperature_img);
+
+  tft.pushImage(5, 120, WATER_DROP_WIDTH, WATER_DROP_HEIGHT, water_drop);
+
+  tft.pushImage(165, 120, WATER_DROP_WIDTH, WATER_DROP_HEIGHT, water_drop);
+
+  WiFi.mode(WIFI_AP_STA);
+
+  esp_wifi_set_channel(3, WIFI_SECOND_CHAN_NONE);
+
   if (esp_now_init() != 0) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-   
+
   // Register callback function
   esp_now_register_recv_cb(OnDataRecv);
-  last_milli = millis();
-  last_sec = millis();
 }
- 
+
 void loop() {
-  if (last_milli + 1000 < millis()) {
-    last_milli = millis(); 
-    ntp.update();
+  aht.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+  tft.drawString(String(humidity.relative_humidity, 1) + " %", 205, 125, 4);
+  //tft.drawString(String(temp.temperature, 1), 194, 180, 6);
+  tft.drawString(String(bmp.readTemperature(), 1), 194, 180, 6);
 
-    tft.setCursor(8, 0);  // Set position (x,y)
-    tft.setTextColor(ST7735_ORANGE, ST7735_BLACK);  // Set color of text. First is the color of text and after is color of background
-    tft.setTextSize(3);  // Set text size. Goes from 0 (the smallest) to 20 (very big)
+  if (print_time) {
+    Serial.println("Sleep MS:" + String(1000 - ((millis() - gotTimeMillis) % 1000)));
+    delay(1000 - ((millis() - gotTimeMillis) % 1000));
 
-    String hours = (ntp.hours() < 10 ? "0" : "") + String(ntp.hours());
-    String minutes = (ntp.minutes() < 10 ? "0" : "") + String(ntp.minutes());
-    String seconds = (ntp.seconds() < 10 ? "0" : "") + String(ntp.seconds());
+    Serial.print("MEZ: "); Serial.println(MEZ);
 
-    tft.println(hours + ":" + minutes + ":" + seconds);
+    Serial.println("Hours: " + String(rawByteToInt(dcf77Hour)));
+    Serial.println("Minutes: " + String(rawByteToInt(dcf77Minute)));
+    Serial.println("Seconds: " + String((millis() - gotTimeMillis) / 1000));
 
-    if (last_sec + 10000 < millis()) {
-      last_sec = millis();
-      tft.setTextSize(1);
-//      tft.fillRect(80, 96, 160, 128, ST7735_BLACK);
-      tft.setTextColor(ST7735_ORANGE, ST7735_BLACK);
+    Serial.print(daysOfTheWeek[rawByteToInt(dcf77DayOfWeek)-1]);
+    Serial.print(", ");
+    Serial.print(String(rawByteToInt(dcf77DayOfMonth)) + ".");
+    Serial.print(String(rawByteToInt(dcf77Month)) + ".");
+    Serial.println(2000 + rawByteToInt(dcf77Year));
 
-      tft.setCursor(115, 80);  // Set position (x,y)
-      tft.println(String(dht.readTemperature() - 1.0, 2) + " C");
-      tft.setCursor(120, 90);  // Set position (x,y)
-      tft.println(String(dht.readHumidity(), 1) + " %");
+    rtc.adjust(DateTime(2000 + rawByteToInt(dcf77Year), rawByteToInt(dcf77Month), rawByteToInt(dcf77DayOfMonth), rawByteToInt(dcf77Hour), rawByteToInt(dcf77Minute), (millis() - gotTimeMillis) / 1000));
+
+    print_time = false;
+  }
+
+  if (print_temp) {
+    // Print to Serial Monitor
+    Serial.println();
+
+    Serial.print("Temperature: ");
+    Serial.println(esp_now_temp);
+    Serial.print("Humidity: ");
+    Serial.println(esp_now_hum);
+    Serial.print("Pressure: ");
+    Serial.println(esp_now_pres);
+
+    Serial.print("ESP NOW ID: ");
+    Serial.println(esp_now_id);
+
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+
+    if (esp_now_id == 0) {
+      tft.drawString(String(esp_now_temp, 1),  34, 180, 6);
+
+      tft.drawString(String(esp_now_hum, 1) + "%",  40, 125, 4);
     }
+    else if (esp_now_id == 1) {
+      tft.drawCentreString( String(esp_now_temp, 1) + 'C',  320/2, 80, 4);
+    }
+    else {
+      tft.drawString("Err",  34, 180, 6);
+
+      tft.drawString("Error",  40, 125, 4);
+    }
+    print_temp = false;
+  }
+
+  for(int i=0; i<10; i++){
+    printLocalTime();
+    delay(1000);
   }
 }
